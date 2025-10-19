8f21084eba96539ae983d8e93118dac99f99bb94
fahimfahad92 <fahim.fahad@toptal.com>
2025-10-19 18:51:49 +0600

    Basic template

---
+++ src/main/java/rnd/fahim/javaspringbootawsrnd/JavaSpringBootAwsRndApplication.java
@@ -0,0 +1,12 @@
+package rnd.fahim.javaspringbootawsrnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootAwsRndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSpringBootAwsRndApplication.class, args);
+  }
+}
